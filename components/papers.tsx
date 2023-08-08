f67379d2f9fa8a9da7b67f3ef83fa2a58f2d6435
Joseph Chee Chang <josephcc@cmu.edu>
2023-08-08 13:01:37 -0700

    minor changes

--- components/papers.tsx
+++ components/papers.tsx
@@ -22,7 +22,7 @@ export default function Papers() {
 
           {/* Testimonials */}
           <div className="max-w-3xl mx-auto mt-20" data-aos="zoom-y-out">
-            <h4 className="h4 mb-2">Project Overview</h4>
+            <h4 className="h4 mb-2">Semantic Reader Project Overview</h4>
             <ul role="list" className="divide-y divide-gray-100">
               <li className="flex justify-between gap-x-6 py-2">
                 <p className="text-s text-gray-600">
@@ -48,7 +48,7 @@ export default function Papers() {
                 </p>
               </li>
             </ul>
-            <h4 className="h4 mb-2 mt-8">Open Research Resources: Libraries, Models, Datasets.</h4>
+            <h4 className="h4 mb-2 mt-8">Open Research Resources: Libraries, Models, Datasets</h4>
             <ul role="list" className="divide-y divide-gray-100">
               <li className="flex justify-between gap-x-6 py-2">
                 <p className="text-s text-gray-600">
