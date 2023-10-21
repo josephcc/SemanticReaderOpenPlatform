fc2934005c8504299d8cef1a61d392db13b35a31
Joseph Chee Chang <josephcc@cmu.edu>
2023-10-20 19:13:13 -0700

    Monkey patching some venue data

--- components/paper.tsx
+++ components/paper.tsx
@@ -5,6 +5,15 @@ export default function Paper({paper}: {paper: PaperType}) {
   if (`${paper.externalIds.CorpusId}` === '261276997' || `${paper.externalIds.CorpusId}` === '260899915') {
     venue = 'The ACM Symposium on User Interface Software and Technology. '
   }
+  if (`${paper.externalIds.CorpusId}` === '215768677' || `${paper.externalIds.CorpusId}` === '215416146') {
+    venue = 'Proceedings of the Annual Meeting of the Association for Computational Linguistics. '
+  }
+  if (`${paper.externalIds.CorpusId}` === '222291111') {
+    venue = 'Proceedings of the First Workshop on Scholarly Document Processing @ ACL. '
+  }
+  if (`${paper.externalIds.CorpusId}` === '216867622') {
+    venue = 'Findings of the Association for Computational Linguistics: EMNLP. '
+  }
   return (
     <>
       <a href={`https://www.semanticscholar.org/paper/${paper.paperId}`} target="_blank" style={{ fontWeight: 600 }}>{paper.title}</a>. {venue}{paper.year}. {paper.authors.map((author) => author.name).join(', ')}.
