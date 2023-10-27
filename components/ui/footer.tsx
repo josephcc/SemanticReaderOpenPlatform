a38cb724f01d80248ebd153b282de91acd46bba9
Joseph Chee Chang <josephcc@cmu.edu>
2023-10-27 14:36:32 -0700

    Minior style change - footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer>
-      <div style={{display: 'flex', fontSize: '10px', color: 'gray', justifyContent: 'center', paddingBottom: '12px'}}>
+      <div style={{display: 'flex', fontSize: '10px', color: 'gray', justifyContent: 'center', paddingBottom: '10px', paddingTop: '10px'}}>
         <div style={{flex: '0 0 560px'}}>
           © <a href="https://allenai.org" target='_blank' rel='noreferrer'>The Allen Institute for Artificial Intelligence (AI2)</a> - All Rights Reserved |&nbsp; 
           <a href="https://allenai.org/privacy-policy.html" target='_blank' rel='noreferrer'>Privacy Policy</a>&nbsp;
