a3d78ccdae069faff06d405079338eace9f9722e
Joseph Chang <josephcc@cmu.edu>
2023-11-13 11:44:21 -0800

    Update paper.tsx

--- components/paper.tsx
+++ components/paper.tsx
@@ -16,7 +16,7 @@ export default function Paper({paper}: {paper: PaperType}) {
   }
   return (
     <span style={{ display: 'inline-block' }}>
-      {paper.externalIds.CorpusId === 256868353 ? <span style={{ fontWeight: 'bold' }}>🏆 Best Paper Award<br /></span> : null}
+      {(paper.externalIds.CorpusId === 256868353 || paper.externalIds.CorpusId === 248420091) ? <span style={{ fontWeight: 'bold' }}>🏆 Best Paper Award<br /></span> : null}
       <a href={`https://www.semanticscholar.org/paper/${paper.paperId}`} target="_blank" style={{ fontWeight: 600 }}>{paper.title}</a><br/>{paper.authors.map((author) => author.name).join(', ')}. {venue}{paper.year}.
       {paper.externalIds.CorpusId === 247187606 ? <span> Presentation at CHI 2024.</span> : null}
     </span>
